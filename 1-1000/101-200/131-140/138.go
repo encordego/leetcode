9ee967f923987e5d9e64b38f391827dc69be6474
encordego <encordego+dev@gmail.com>
2023-09-12 22:26:01 +0300

    feat: add solution to No.138

---
+++ 1-1000/101-200/131-140/138.go
@@ -0,0 +1,30 @@
+package main
+
+// 138. Copy List with Random Pointer
+//
+// https://leetcode.com/problems/copy-list-with-random-pointer/
+func copyRandomList(head *Node) *Node {
+	if head == nil {
+		return nil
+	}
+	storage, current := make(map[*Node]*Node), head
+	for current != nil {
+		storage[current] = &Node{Val: current.Val}
+		current = current.Next
+	}
+	current = head
+	for current != nil {
+		storage[current].Next = storage[current.Next]
+		storage[current].Random = storage[current.Random]
+		current = current.Next
+	}
+
+	return storage[head]
+}
+
+// Definition for a Node.
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
